c128affe92930a72b95833edef9f2ce1df267651
Lana <lana.12@wanadoo.fr>
2024-05-27 14:30:12 +0200

    Reorganization of data files + rewriting of functions

--- src/models/category.php
+++ src/models/category.php
@@ -20,10 +20,11 @@ function getCategories() {
             ];
         }
         fclose($file_pointer);
+        
         return $categories;
 
     } else {
-        echo 'Oups, une erreur est survenue lors de l\'ouverture du fichier !!!';
+        echo 'Oups, une erreur est survenue lors de l\'ouverture du fichier des catégories !!!';
     }
 }
 
@@ -42,85 +43,29 @@ function getCategoryById($id) {
 }
 
 
-// Retrieve undercategories
-function getUnderCategories() {
-    $undercategories = [];
-
-    $file_path= dirname(__FILE__) .'/../datas/undercategories.csv';
-
-    if (file_exists(($file_path))) {
-        $file_pointer = fopen($file_path, 'r');
-
-        while ($data = fgetcsv($file_pointer, null, ';')) {
-            $undercategories[]= [
-                'id'=> $data[0],
-                'title'=> $data[1],
-                'description'=> $data[2],
-                'category_id'=> $data[3],
-            ];
-        }
-        fclose($file_pointer);
-        return $undercategories;
-
-    } else {
-        echo 'Oups, une erreur est survenue lors de l\'ouverture du fichier !!!';
-    }
-   
-}
-
-
-// Retrieve name of the undercategory by id
-function getNameUnderCategoryById($id) {
-    $underCategories = getUnderCategories(); 
-
-    foreach ($underCategories as $underCategory) {
-        if ($underCategory['id'] == $id) {
-            return $underCategory;
-        }
-    }
-
-    return null; 
-}
-
 
 // Retrieve data of the file csv
-function getDataFromCsv($filename) {
-    $file_path = dirname(__FILE__) . '/../datas/' . $filename;
+function getDataFromCsv($folderName, $fileName) {
+    $file_path = dirname(__FILE__) . '/../datas/' . $folderName.'/'.$fileName.'.csv';
     $dataCsv = [];
-    $specialLinks = [];
 
     if (file_exists($file_path)) {
         $file_pointer = fopen($file_path, 'r');
 
         while ($data = fgetcsv($file_pointer, null, ';')) {
-            if ($data[0] == 0) {
                 
-                // Ligne avec id=0, traiter comme un lien spécial
-                $specialLinks[] = [
-                    'id' => $data[0],
-                    'title' => $data[1],
-                    'link' => [
-                        $data[2],
-                        $data[3],
-                        $data[4],
-                        $data[5],
-                    ]
-                ];
-                
-            } else {
-                // Autres lignes, traiter comme des données régulières
-                $dataCsv[] = [
-                    'id' => $data[0],
-                    'title' => $data[1],
-                    'description' => $data[2],
-                    'underCategory_id' => $data[3]
-                ];
-            }
+            $dataCsv[] = [
+                'id' => $data[0],
+                'title' => $data[1],
+                'description' => $data[2],
+                'underCategory_id' => $data[3]
+            ];
+            
         }
         fclose($file_pointer);
     } else {
-        echo 'Oups, une erreur est survenue lors de l\'ouverture du fichier !!!';
+        echo 'Oups, une errrreur est survenue lors de l\'ouverture du fichier !!!';
     }
 
-    return ['data' => $dataCsv, 'specialLinks' => $specialLinks];
+    return $dataCsv;
 }
---
+++ src/models/underCategory.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../utils/functions.php');
+
+
+
+// Retrieve undercategories
+function getUnderCategories($folder) {
+    $undercategories = [];
+    $introCategorie = [];
+
+    // $file_path= dirname(__FILE__) .'/../datas/undercategories.csv';
+    $file_path= dirname(__FILE__) .'/../datas/'.$folder.'/undercategories.csv';
+
+    if (file_exists(($file_path))) {
+        $file_pointer = fopen($file_path, 'r');
+
+        while ($data = fgetcsv($file_pointer, null, ';')) {
+
+            if ($data[0] == 0){
+                $introCategorie[] = [
+                    'id' => $data[0],
+                    'intro' => $data[1],
+                ];
+
+            } else {
+                $undercategories[]= [
+                    'id'=> $data[0],
+                    'title'=> $data[1],
+                    'description'=> $data[2],
+                    'category_id'=> $data[3],
+                ];
+
+            }
+            
+        }
+        fclose($file_pointer);
+
+    } else {
+        echo 'Oups, une erreur est survenue lors de l\'ouverture du fichier !!!';
+    }
+    
+    return ['data' => $undercategories, 'introCategorie' => $introCategorie];
+
+   
+}
+
+
+// Retrieve name of the undercategory by id
+function getNameUnderCategoryById($id, $folder) {
+    $results = getUnderCategories($folder); 
+    $underCategories = $results['data']; 
+
+    foreach ($underCategories as $underCategory) {
+        if ($underCategory['id'] == $id) {
+            return $underCategory;
+        }
+    }
+
+    return null; 
+}
